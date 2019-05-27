e8c1a7cfbb88b0112ca8c34791c38d4983da42e4
Warrick FitzGerald <warrick@eosnewyork.io>
2019-05-27 13:13:27 -0400

    1st API call working

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,159 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+
+
+const client = new ApolloClient({
+  uri: "https://api-useast.graphcms.com/v1/cjw55u46y3t0201ehvewzsi7m/master"
+});
+
+
+const Cuts = () => (
+    <Query
+      query={gql`
+          query content($first: Int, $skip: Int, $where: PatternWhereInput, $orderBy: PatternOrderByInput) {
+                patterns: patternsConnection(first: $first, skip: $skip, where: $where, orderBy: $orderBy) {
+                  edges {
+                    node {
+                      status
+                      updatedAt
+                      createdAt
+                      id
+                      name
+                      images {
+                        status
+                        updatedAt
+                        createdAt
+                        id
+                        handle
+                        fileName
+                        height
+                        width
+                        size
+                        mimeType
+                      }
+                      tools
+                      instructions
+                      breed {
+                        id
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+      `}
+    >
+      {({ loading, error, data } : any) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+  
+        return data.patterns.edges.map(({ node } : any) => (
+          <div key={node.name}>
+            <Link to={`/cut/${node.id}`}>{node.breed.name} - {node.name}</Link>
+          </div>
+        ));
+      }}
+    </Query>
+  );
+
+
+  interface Variables {
+    where: string;
+  };
+
+  const x : Variables = {where: "id=cjw56uher4awf0946vyhwiuno"};
+
+  const Cut = () => (
+    <Query 
+        variables={x}
+        query={gql`
+          query content($first: Int, $skip: Int, $where: PatternWhereInput, $orderBy: PatternOrderByInput) {
+                patterns: patternsConnection(first: $first, skip: $skip, where: $where, orderBy: $orderBy) {
+                  edges {
+                    node {
+                      status
+                      updatedAt
+                      createdAt
+                      id
+                      name
+                      images {
+                        status
+                        updatedAt
+                        createdAt
+                        id
+                        handle
+                        fileName
+                        height
+                        width
+                        size
+                        mimeType
+                      }
+                      tools
+                      instructions
+                      breed {
+                        id
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+      `}
+    >
+      {({ loading, error, data } : any) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+  
+        return data.patterns.edges.map(({ node } : any) => (
+          <div key={node.name}>
+            <Link to={`/cut/${node.id}`}>{node.breed.name} - {node.name}</Link>
+          </div>
+        ));
+      }}
+    </Query>
+  );
+
+const Index : any = () => {
+    return(
+        <div>
+            <h2>Grooming Fieldguide</h2>
+            <Cuts/>
+        </div>
+    )
+}
+
+const CutInfo : any  = ({match} : any) => {
+    return(
+        <div>
+            Cut info {match.params.cutId}
+            <Cut/>
+        </div>
+    )
+}
+
+const App = () => (
+    <ApolloProvider client={client}>
+
+        <Router>
+            <div>
+                <nav>
+                    <Index/>
+                </nav>
+                <Route path="/cut/:cutId" component={CutInfo} />             
+            </div>
+        </Router>
+
+
+    </ApolloProvider>
+  );
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
